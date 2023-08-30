424c37faab836959ab147a4f1955dcb123eda294
Oscar Gu <oscar.gu@philips.com>
2023-08-30 18:30:47 +0800

    1.0.3

--- src/Dao.LightFramework/Common/Utilities/StaticLogger.cs
+++ src/Dao.LightFramework/Common/Utilities/StaticLogger.cs
@@ -9,7 +9,7 @@ public class StaticLogger
     public static void LogInformation(string message, params object[] args) => Logger?.LogInformation(message, args);
     public static void LogWarning(string message, params object[] args) => Logger?.LogWarning(message, args);
     public static void LogError(string message, params object[] args) => Logger?.LogError(message, args);
-    public static void LogError(Exception ex, string message = null, params object[] args) => Logger?.LogError(ex, message, args);
+    public static void LogError(Exception ex, string message = "", params object[] args) => Logger?.LogError(ex, message, args);
     public static void LogDebug(string message, params object[] args) => Logger?.LogDebug(message, args);
     public static void LogDebug(Func<string> messageFunc) => Logger.Debug(messageFunc);
     public static T ExecIf<T>(LogLevel level, Func<T> execFunc) => Logger.ExecIf(level, execFunc);
