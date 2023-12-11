c47fca4053795fb70a99100863f4ee981355a574
Oscar Gu <oscar.gu@philips.com>
2023-12-11 22:31:54 +0800

    1.0.23

---
+++ src/Dao.LightFramework/HttpApi/Utilities/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dao.LightFramework.HttpApi.Utilities;
+
+public static class Extensions
+{
+    public static IServiceCollection AddConfiguration(this IServiceCollection services, Func<IConfigurationRoot, IConfigurationRoot> createConfiguration)
+    {
+        if (services == null || createConfiguration == null)
+            return services;
+
+        var builder = new ConfigurationBuilder();
+        builder.AddJsonFile("appsettings.json", true, true);
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+        builder.AddJsonFile($"appsettings.{environment}.json", true, true);
+        var configuration = builder.Build();
+        var newConfiguration = createConfiguration(configuration);
+        if (newConfiguration == null)
+            return services;
+
+        var newConfig = new ConfigurationBuilder();
+        using (var scope = services.BuildServiceProvider().CreateScope())
+        {
+            var config = scope.ServiceProvider.GetService<IConfiguration>();
+            newConfig.AddConfiguration(config);
+            newConfig.AddConfiguration(newConfiguration);
+        }
+
+        return services.AddSingleton<IConfiguration>(newConfig.Build());
+    }
+}
