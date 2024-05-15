d0a0bbe095a1e1e6c54ff416de3a6d3d3c679ff9
oscar <oscar@oscar.com>
2024-05-15 14:29:26 +0800

    1.0.24+

--- src/Dao.LightFramework/HttpApi/Utilities/JsonIgnoreContractResolver.cs
+++ src/Dao.LightFramework/HttpApi/Utilities/JsonIgnoreContractResolver.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Collections.Concurrent;
+using System.Reflection;
 using Dao.LightFramework.Common.Utilities;
 using Dao.LightFramework.Domain.Entities;
 using Newtonsoft.Json;
@@ -8,16 +9,31 @@ namespace Dao.LightFramework.HttpApi.Utilities;
 
 public class JsonIgnoreContractResolver : CamelCasePropertyNamesContractResolver
 {
+    static readonly ConcurrentDictionary<Tuple<Type, string>, MethodInfo> methods = new ();
+
+    static MethodInfo GetShouldSerializeMethod(Type type, string name) => methods.GetOrAdd(new Tuple<Type, string>(type, name), k =>
+    {
+        var method = k.Item1.GetMethod("ShouldSerialize" + k.Item2, Type.EmptyTypes);
+        return method != null && method.ReturnType == typeof(bool) ? method : null;
+    });
+
     protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
     {
         var property = base.CreateProperty(member, memberSerialization);
         var type = member.DeclaringType;
 
-        if (property.ShouldSerialize == null 
+        if (property.ShouldSerialize == null
             && (typeof(IDomainSite).IsAssignableFrom(type) && member.Name.In(nameof(IDomainSite.Domain), nameof(IDomainSite.Site))
                 || typeof(IMutable).IsAssignableFrom(type) && member.Name.In(nameof(IMutable.CreateUser), nameof(IMutable.CreateTime), nameof(IMutable.UpdateUser), nameof(IMutable.UpdateTime))))
         {
-            property.ShouldSerialize = _ => false;
+            property.ShouldSerialize = instance =>
+            {
+                if (instance == null)
+                    return false;
+
+                var method = GetShouldSerializeMethod(instance.GetType(), member.Name);
+                return method != null && Convert.ToBoolean(method.Invoke(instance, null));
+            };
         }
 
         if (property.PropertyType == typeof(TimeSpan))
