b1339755201b983bafa67f57a8b10fec157a6743
oscar <oscar@oscar.com>
2024-08-22 14:59:49 +0800

    1.0.30

--- src/Dao.LightFramework/Common/Utilities/SeriLogger.cs
+++ src/Dao.LightFramework/Common/Utilities/SeriLogger.cs
@@ -8,17 +8,36 @@ public class SeriLoggerSetting
 {
     public static Func<string, ILogger> CreateLogger { get; set; }
     public static LogEventLevel LogLevel { get; set; } = LogEventLevel.Verbose;
+    public static string OutputTemplate { get; set; } = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
     public static long? FileSizeLimitBytes { get; set; } = 1073741824;
+    public static RollingInterval RollingInterval { get; set; } = RollingInterval.Hour;
     public static int? RetainedFileCountLimit { get; set; } = 168;
 
-    public static LoggerConfiguration CreateDefaultConfiguration(string serviceName, LogEventLevel? logLevel = null, long? fileSizeLimitBytes = null, int? retainedFileCountLimit = null) =>
+    public static LoggerConfiguration CreateDefaultConfiguration(string serviceName,
+        LogEventLevel? logLevel = null,
+        string outputTemplate = null,
+        long? fileSizeLimitBytes = null,
+        RollingInterval? rollingInterval = null,
+        int? retainedFileCountLimit = null) =>
         new LoggerConfiguration()
             .MinimumLevel.Is(logLevel ?? LogLevel)
             .WriteTo.Console(LogEventLevel.Information, $"[{{Timestamp:HH:mm:ss}} {{Level:u3}}] ({serviceName}) {{Message:lj}}{{NewLine}}")
-            .WriteTo.File($"./Logs/{serviceName.ToLowerInvariant()}_log_.txt", logLevel ?? LogLevel, fileSizeLimitBytes: fileSizeLimitBytes ?? FileSizeLimitBytes, shared: true, rollingInterval: RollingInterval.Hour, rollOnFileSizeLimit: true, retainedFileCountLimit: retainedFileCountLimit ?? RetainedFileCountLimit);
+            .WriteTo.File($"./Logs/{serviceName.ToLowerInvariant()}_log_.txt",
+                logLevel ?? LogLevel,
+                outputTemplate ?? OutputTemplate,
+                fileSizeLimitBytes: fileSizeLimitBytes ?? FileSizeLimitBytes,
+                shared: true,
+                rollingInterval: rollingInterval ?? RollingInterval,
+                rollOnFileSizeLimit: true,
+                retainedFileCountLimit: retainedFileCountLimit ?? RetainedFileCountLimit);
 
-    public static ILogger CreateDefaultLogger(string serviceName, LogEventLevel? logLevel = null, long? fileSizeLimitBytes = null, int? retainedFileCountLimit = null) =>
-        CreateDefaultConfiguration(serviceName, logLevel, fileSizeLimitBytes, retainedFileCountLimit).CreateLogger();
+    public static ILogger CreateDefaultLogger(string serviceName,
+        LogEventLevel? logLevel = null,
+        string outputTemplate = null,
+        long? fileSizeLimitBytes = null,
+        RollingInterval? rollingInterval = null,
+        int? retainedFileCountLimit = null) =>
+        CreateDefaultConfiguration(serviceName, logLevel, outputTemplate, fileSizeLimitBytes, rollingInterval, retainedFileCountLimit).CreateLogger();
 }
 
 public class SeriLogger<TService>
